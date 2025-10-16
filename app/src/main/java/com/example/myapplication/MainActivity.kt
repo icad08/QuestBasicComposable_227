c6f67625226f09f3ac7672f11072b60f7f63c610
icad08 <irsyadrm08@gmail.com>
2025-10-16 16:04:07 +0700

    mengatur Scaffold untuk layout dasar aplikasi

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,3 +15,26 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+package com.example.mylayout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.mylayout.ui.theme.MyLayoutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyLayoutTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    // Konten akan ditempatkan di sini
+                }
+            }
+        }
+    }
+}
